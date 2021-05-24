2f03dadbd344dd8fc5ccac28e088cdf02e340eb6
Shikai1997 <56082441+Shikai1997@users.noreply.github.com>
2021-05-24 21:44:57 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <div>
-    <Footer></Footer>
+  <div :class="$style.mask">
+    <!-- <Footer></Footer> -->
+    <div :class="$style.loading">
+      <img :src="require('@/assets/images/gif/Eevee.gif')" alt="" />
+      <img :src="require('@/assets/images/gif/sylveon.gif')" alt="" />
+      <img :src="require('@/assets/images/gif/Eevee.gif')" alt="" />
+      <img :src="require('@/assets/images/gif/sylveon.gif')" alt="" />
+      <img :src="require('@/assets/images/gif/Eevee.gif')" alt="" />
+      <img :src="require('@/assets/images/gif/sylveon.gif')" alt="" />
+    </div>
   </div>
 </template>
 <script>
-import Footer from "@/components/layout/footer.vue";
+// import Footer from "@/components/layout/footer.vue";
 export default {
   components: {
-    Footer,
+    // Footer,
   },
 };
 </script>
 
 <style lang="scss" module>
 @import "@/assets/scss/layout/_layout.scss";
+.mask {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
